c2653266ae0d01e7adb6b3579b348b8356f4fba0
Sandop <dpjsdu@163.com>
2018-09-05 14:40:43 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,52 @@
-'use strict'
+'use strict';
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
 
+    //入口
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        //所有产出资源路径
+        path: path.join(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [{
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
+            },
+            {
+                test: /\.(jpg|png|svg|ttf|woff|woff2|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 4096, //4096字节以上生成文件，否则base6
+                    name: '[name].[ext]'
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015'], //关键字
+                    plugins: ['transform-runtime'], //函数
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
