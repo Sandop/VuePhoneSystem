f3a1bb07903383e5443ce3c01571c74639076dc8
Sandop <dpjsdu@163.com>
2018-09-05 14:50:07 +0800

    set main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,35 @@
 'use strict'
 
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+
+import Axios from 'axios';
+Vue.prototype.$ajax = Axios;
+Axios.defaults.baseURL = 'http://182.254.146.100:8899/api/';
+
+
+import App from './app.vue';
+
+
+let router = new VueRouter({
+    linkActiveClass:'mui-active',
+    routes: [
+        //VueRouter：配置路由规则
+        { 
+            path: '/',
+            redirect: { 
+                name: 'home' 
+            } 
+        }, //重定向
+        
+    ]
+});
+
+//创建vue实例
+new Vue({
+    el: '#app',
+    router,
+    render: c => c(App)
+})
