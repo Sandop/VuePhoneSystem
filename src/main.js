4c855bc962786cdeb43b083f3248debb625bb190
Sandop <dpjsdu@163.com>
2018-09-12 15:03:45 +0800

    添加mainjs详细注释

--- src/main.js
+++ src/main.js
@@ -4,14 +4,19 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
-import Axios from 'axios';
-
 import MintUI from 'mint-ui';
 import 'mint-ui/lib/style.css';
 Vue.use(MintUI);
+
+import Axios from 'axios';
+
+import Moment from 'moment';
+
+//css引入
 import './static/vender/mui/dist/css/mui.css';
 import './static/css/reset.css';
 
+//组件引入
 import App from './app.vue';
 import Home from './components/home/home.vue';
 import Member from './components/member/member.vue';
@@ -22,11 +27,14 @@ import NewsDetails from './components/news/newsdetails.vue';
 import NavBar from './components/common/navBar.vue';
 import PhotoShare from './components/photo/share.vue';
 
-import Moment from 'moment';
 
+//Axios配置相关项
 Vue.prototype.$axios = Axios;
+
+//配置基础路径
 Axios.defaults.baseURL = 'http://visney.cn:81';
 
+//配置拦截器实现加载动画，使用mint-ui实现
 Axios.interceptors.request.use( function (config) {
     MintUI.Indicator.open({
         text: '加载中...',
@@ -43,12 +51,21 @@ Axios.interceptors.response.use( function (config){
 });
 
 
+
+//全局组件部分
+
 Vue.component('navBar',NavBar);
 
+//全局组件部分结束
+
+//过滤器
+//使用moment实现时间日期过滤功能
 Vue.filter('converData',function(value){
     return Moment(value).format('YYYY-MM-DD');
 })
 
+
+//路由相关
 let router = new VueRouter({
     linkActiveClass:'mui-active',
     routes: [
